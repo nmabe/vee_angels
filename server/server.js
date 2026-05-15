d5c1f00f92833cf37bf8467117a12d89e9b01230
Nkululeko Mokgele <nmabe@student.wethinkcode.co.za>
2026-05-15 17:21:32 +0200

    Update CORS origin to production URL

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://vee-angels.vercel.app',
     methods: ['GET','POST','DELETE', 'PUT'],
     allowedHeaders: [
         'Content-Type',

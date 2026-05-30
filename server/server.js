21e9db69622bbf5f8071f4baf83405fa2b9186ba
Nkululeko Mokgele <nmabe@student.wethinkcode.co.za>
2026-05-30 04:50:51 +0200

    Update CORS origin to allow localhost and production

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://vee-angels.vercel.app',
+    origin: [
+        'http://localhost:5173/',
+        'https://vee-angels.vercel.app'
+    ],
     methods: ['GET','POST','DELETE', 'PUT'],
     allowedHeaders: [
         'Content-Type',
